7560eb391394ce371ef1e211ba0fee1aab220a82
Samidul Islam <mohammadsamidulislam58@gmail.com>
2026-07-01 23:17:50 +0600

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int cnt = 0;
+        for(int i = 0; i<prices.size() - 1 ;i++){
+            for(int  j = i+1;j<prices.size(); j++){
+                cnt = max(cnt, (prices[j] - prices[i]));
+            }
+        }
+        return cnt;
+    }
+};
